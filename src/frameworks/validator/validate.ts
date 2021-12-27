e25c588c553983afa58f2897e3c92b873a43d60a
Miguel Angel Martin <mmartinsolis.dev@gmail.com>
2021-12-27 17:00:56 -0500

    Update validate.ts

--- src/frameworks/validator/validate.ts
+++ src/frameworks/validator/validate.ts
@@ -8,7 +8,7 @@ const validate = async (req: any) => {
     await body([
       'name',
       'lastname',
-    ]).notEmpty().withMessage('Data required').isString().withMessage('Data must be string').trim().isLength({max: 30}).withMessage('Data to long').run(req);
+    ]).notEmpty().withMessage('Data required').isString().withMessage('Data must be string').trim().isLength({max: 30}).withMessage('Data to long').trim().escape().run(req);
     await body('birthdate').notEmpty().withMessage('Date required').isDate({format: 'YYYY-MM-DD', strictMode: true}).withMessage('Invalid date').trim().run(req);
     await body([
       'sex',
@@ -19,25 +19,23 @@ const validate = async (req: any) => {
       'relationship',
       'contactEmail',
       'medicalInformation'
-    ]).isString().withMessage('Data must be string').trim().run(req);
+    ]).isString().withMessage('Data must be string').trim().escape().run(req);
     await body('status').isBoolean().withMessage("Invalid status").run(req);
-    await body('email').notEmpty().withMessage('Email required').isEmail().withMessage('Invalid Email').trim().normalizeEmail().run(req);
+    await body('email').notEmpty().withMessage('Email required').isEmail().withMessage('Invalid Email').trim().normalizeEmail().escape().run(req);
     await body([
       'cellphone',
       'homephone',
       'contactCellPhone',
       'contactHomePhone'
-    ]).isNumeric().withMessage('Invalid number').run(req);
+    ]).isNumeric().withMessage('Invalid number').escape().run(req);
     break;
-
 
     case '/singUp':
     await body('email').notEmpty().withMessage('Email required').isEmail().withMessage('Invalid Email').trim().normalizeEmail().run(req);
-    await body('password').notEmpty().withMessage('Password required').isStrongPassword({minLength: 8}).withMessage('Password is not secure').trim().run(req);
+    await body('password').notEmpty().withMessage('Password required').isStrongPassword().withMessage('The password must be at least 8 characters long, constains At least one uppercase, At least one lower case. At least one special character and one number.').trim().run(req);
     await body([
       'firstname',
-      'lastname',
-      'role'
+      'lastname'
     ]).notEmpty().withMessage('Data required').isString().withMessage('Data must be string').trim().escape().run(req);
     break;
   }
